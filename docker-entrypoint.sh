078194d97932f6c7c9e835f0603681d8eb4dec4b
Johannes Rumpf <johannes.rumpf@gmail.com>
2025-11-24 22:07:19 +0100

    update docker cache problems

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -30,6 +30,8 @@ while IFS= read -r -d '' file; do
                     printf "DEBUG file added.\n";;
     esac
 done
+rm -Rf /app/cache/*
+chmod 777 /app/cache
 
 # This feature can set the internal port that apache uses to something else.
 # If docker is run on network:service mode, no two containers can use port 80
